a5a1226a5ce451e7413671ad90c30360d2f4bc22
Won Doyoung <dywon93@naver.com>
2023-02-26 17:39:58 +0900

    allowed host 수정

--- DY_WEB/settings.py
+++ DY_WEB/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-7)h=vy4+*xuek2_knq$5tu%*#o)^yija*_qpt3-($!@fvapwxs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 # Application definition
 
