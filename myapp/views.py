9e6e1837659c7da371713a29941691af37490a2c
Won Doyoung <dywon93@naver.com>
2023-02-26 17:25:39 +0900

    add tmeplates, html

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
 
-# Create your views here.
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
+
+
+def post_list(request):
+    return render(request, 'myapp/post_list.html', {})
